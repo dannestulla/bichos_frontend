9d3f18ade496f392ef58d321ca6dbf24b0b33f8a
DannEstulla <dannestulla@gmail.com>
2024-05-18 19:54:44 -0300

    feat: added config file

---
+++ lib/config.dart
@@ -0,0 +1,21 @@
+// Instagram Pages added to main screen
+const pages = [
+  "acheseudogulbra",
+  "acheseupetrs",
+  "animaisresgatadosmathias",
+  "caesresgatadoscanoas",
+  "dogs_da_acucena",
+  "meubichotasalvocanoas",
+  'onlycats.canoas',
+  "petresgatado_canoas",
+  "resgatadoagoracanoas",
+  "resgatados.pasqualini",
+  "resgatadosnazario409",
+  "tosalvo.pet"
+];
+
+// Mode to create a list of ignored images
+const ignoreModeOn = false;
+
+// Size of the page when loading main screen, this includes .jpg and .txt
+const pageSize = 50;
